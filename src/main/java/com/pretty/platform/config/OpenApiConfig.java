e130d51838c1b7a586057337f3c2df81060e8940
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-12-11 14:33:56 -0500

    Add .gitignore and OpenAPI configuration for Swagger documentation

---
+++ src/main/java/com/pretty/platform/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.pretty.platform.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pretty Platform API")
+                        .description("API documentation for Pretty company platform")
+                        .version("1.0.0"));
+    }
+}
